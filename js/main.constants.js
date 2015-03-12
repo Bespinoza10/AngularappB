0ce6f046f0a9633fc8db3b8b7a8d3736b02ea12b
Brandon Espinoza <espinozab100@gmail.com>
2015-03-12 12:27:50 -0500

    Added All my depencies (bootstrap,jquery,firebase,angualr) and also added buttons and table. Finished projects

---
+++ js/main.constants.js
@@ -0,0 +1,17 @@
+angular
+  .module('tas')
+  .constant('BASE_URL', 'https://tangularlist.firebaseio.com')
+  .constant('COHORT_OPTIONS', [
+    'Full Stack Developer',
+    'Web Designer',
+    'N/A',
+    'Back End Developer',
+    'Front End Developer',
+    'Teacher',
+    'Window Washer',
+    'Electrician',
+    'Plumber',
+    'Barber',
+    'Hair Specialists'
+  ]);
+	
